0b8a256f6d74e99625578a6cbac574e1a6bc1149
aleksandr.galiew@gmail.com <aleksandr.galiew@gmail.com>
2024-08-07 23:15:14 +0300

    Pop-itka v nasledovanie OOP

---
+++ Project3/childClass.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "addedClass.h"
+ref struct childClass :
+    public addedClass
+{
+private:
+    /*childClass()  { std::cout << "childClass()" << std::endl;  }
+    ~childClass() { std::cout << "~childClass()" << std::endl; }*/
+
+
+public:
+
+};
+
