2b389aac158183cd4e4f55ef7abd9109d34c5075
aleksandr.galiew@gmail.com <aleksandr.galiew@gmail.com>
2024-08-07 22:02:15 +0300

    addedClass is work

--- Project3/addedClass.h
+++ Project3/addedClass.h
@@ -1,5 +1,15 @@
+#include <iostream>
+
 #pragma once
 ref class addedClass
 {
+public:
+	addedClass()  { std::cout << "addedClass()" << std::endl;  }
+	~addedClass() { std::cout << "~addedClass()" << std::endl; }
+
+	float tripling(float n) { return n * 3; }
+private:
+	int inA;
+	int inB;
 };
 
